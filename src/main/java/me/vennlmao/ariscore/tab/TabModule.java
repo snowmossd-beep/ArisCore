5400cb8ee7257f6ae3951852ff7299eebe84d9ba
snowmossd-beep <snowmossd@gmail.com>
2026-09-24 02:24:37 +0700

    Update TabModule.java

--- src/main/java/me/vennlmao/ariscore/tab/TabModule.java
+++ src/main/java/me/vennlmao/ariscore/tab/TabModule.java
@@ -2,15 +2,11 @@ package me.vennlmao.ariscore.tab;
 
 import me.vennlmao.ariscore.ArisCore;
 import me.vennlmao.ariscore.tab.commands.TabCommand;
-import me.vennlmao.ariscore.tab.managers.BelownameManager;
 import me.vennlmao.ariscore.tab.managers.ConditionEvaluator;
-import me.vennlmao.ariscore.tab.managers.NameTagManager;
 import me.vennlmao.ariscore.tab.managers.PapiManager;
 import me.vennlmao.ariscore.tab.managers.ScoreboardManager;
 import me.vennlmao.ariscore.tab.managers.TabConfigManager;
 import me.vennlmao.ariscore.tab.managers.TabListManager;
-import org.bukkit.Bukkit;
-import org.bukkit.scoreboard.Scoreboard;
 import me.vennlmao.ariscore.LicenseManager;
 
 public class TabModule {
@@ -21,9 +17,6 @@ public class TabModule {
     private ConditionEvaluator conditionEvaluator;
     private TabListManager tabListManager;
     private ScoreboardManager scoreboardManager;
-    private NameTagManager nameTagManager;
-    private BelownameManager belownameManager;
-    private Scoreboard sharedScoreboard;
 
     public TabModule(ArisCore plugin) {
         this.plugin = plugin;
@@ -34,7 +27,6 @@ public class TabModule {
             plugin.getLogger().severe("[ArisCore] TabModule disabled: invalid license.");
             return;
         }
-        sharedScoreboard   = Bukkit.getScoreboardManager().getNewScoreboard();
         configManager      = new TabConfigManager(plugin);
         configManager.load();
 
@@ -42,18 +34,12 @@ public class TabModule {
         conditionEvaluator = new ConditionEvaluator(papiManager);
         tabListManager     = new TabListManager(plugin, papiManager, conditionEvaluator, configManager);
         scoreboardManager  = new ScoreboardManager(plugin, papiManager, conditionEvaluator, configManager);
-        nameTagManager     = new NameTagManager(plugin, papiManager, configManager, sharedScoreboard);
-        belownameManager   = new BelownameManager(plugin, papiManager, configManager, sharedScoreboard);
 
         plugin.getServer().getPluginManager().registerEvents(tabListManager,    plugin);
         plugin.getServer().getPluginManager().registerEvents(scoreboardManager, plugin);
-        plugin.getServer().getPluginManager().registerEvents(nameTagManager,    plugin);
-        plugin.getServer().getPluginManager().registerEvents(belownameManager,  plugin);
 
         tabListManager.start();
         scoreboardManager.start();
-        nameTagManager.start();
-        belownameManager.start();
 
         TabCommand cmd = new TabCommand(this);
         if (plugin.getCommand("tab") != null) {
@@ -67,8 +53,6 @@ public class TabModule {
     public void disable() {
         if (tabListManager    != null) tabListManager.stop();
         if (scoreboardManager != null) scoreboardManager.stop();
-        if (nameTagManager    != null) nameTagManager.stop();
-        if (belownameManager  != null) belownameManager.stop();
         plugin.getLogger().info("[Tab] Module disabled.");
     }
 
@@ -76,15 +60,10 @@ public class TabModule {
         configManager.load();
         tabListManager.reload();
         scoreboardManager.reload();
-        nameTagManager.reload();
-        belownameManager.reload();
     }
 
     public TabConfigManager  getConfigManager()      { return configManager; }
     public PapiManager       getPapiManager()        { return papiManager; }
     public TabListManager    getTabListManager()     { return tabListManager; }
     public ScoreboardManager getScoreboardManager()  { return scoreboardManager; }
-    public NameTagManager    getNameTagManager()     { return nameTagManager; }
-    public BelownameManager  getBelownameManager()   { return belownameManager; }
-    public Scoreboard        getSharedScoreboard()   { return sharedScoreboard; }
-                }
+    }
