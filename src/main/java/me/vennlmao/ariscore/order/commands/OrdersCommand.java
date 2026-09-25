ce5d0d8a3dcc1b2cb45540a421696c3670087dd6
snowmossd-beep <snowmossd@gmail.com>
2026-09-25 17:17:58 +0700

    Update OrdersCommand.java

--- src/main/java/me/vennlmao/ariscore/order/commands/OrdersCommand.java
+++ src/main/java/me/vennlmao/ariscore/order/commands/OrdersCommand.java
@@ -1,7 +1,7 @@
 package me.vennlmao.ariscore.order.commands;
 
+import me.vennlmao.ariscore.order.OrderModule;
 import me.vennlmao.ariscore.order.managers.OrdersService;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,13 +10,24 @@ import org.jetbrains.annotations.NotNull;
 
 public final class OrdersCommand implements CommandExecutor {
    private final OrdersService ordersService;
+   private final OrderModule module;
 
-   public OrdersCommand(OrdersService ordersService) {
+   public OrdersCommand(OrdersService ordersService, OrderModule module) {
       this.ordersService = ordersService;
+      this.module = module;
    }
 
    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-      if (sender instanceof Player player) {
+      if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+         if (!sender.hasPermission("ariscore.order.admin")) {
+            sender.sendMessage(this.ordersService.noPermissionMessage());
+            return true;
+         }
+
+         this.module.reload();
+         sender.sendMessage(this.ordersService.reloadedMessage());
+         return true;
+      } else if (sender instanceof Player player) {
          this.ordersService.resetSession(player);
          this.ordersService.openOrders(player);
          return true;
@@ -24,4 +35,4 @@ public final class OrdersCommand implements CommandExecutor {
          return true;
       }
    }
-}
+                 }
